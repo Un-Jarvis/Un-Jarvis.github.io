90779c74ed7aea467e0aa9479cda6ffed2ee83a8
Jarvis <unthinkable1218@gmail.com>
2023-02-27 03:19:46 +0800

    Added infobar.js

---
+++ javascripts/rootInfobar.js
@@ -0,0 +1,24 @@
+var infobar = document.getElementsByClassName("infobar")[0];
+infobar.innerHTML = `
+    <a href="https://github.com/Un-Jarvis" target="_blank">
+        <img class="social" src="images/logo/github.png" alt="github" />
+    </a>
+    <a href="https://www.linkedin.com/in/yuhua-huang-085432185/" target="_blank">
+        <img class="social" src="images/logo/linkedin.png" alt="linkedin" />
+    </a>
+    <a href="https://www.weibo.com/unthinkable1218" target="_blank">
+        <img class="social" src="images/logo/weibo.png" alt="weibo" />
+    </a>
+    <a href="https://discord.gg/qnW7gKA" target="_blank">
+        <img class="social" src="images/logo/discord.png" alt="discord" />
+    </a>
+    <a href="https://www.twitch.tv/u_jarvis_n" target="_blank">
+        <img class="social" src="images/logo/twitch.png" alt="twitch" />
+    </a>
+    <a href="https://space.bilibili.com/5853243" target="_blank">
+        <img class="social" src="images/logo/bilibili.png" alt="bilibili" />
+    </a>
+    <a href="https://www.youtube.com/channel/UC_DXsFQ5W2TJYd_SQTxenGA" target="_blank">
+        <img class="social" src="images/logo/youtube.png" alt="youtube" />
+    </a>
+`;
